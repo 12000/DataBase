e6bef5c4c9bfdddc2f5376aa31820ee35cec001f
Nikitin Egor <egor777@izh.com>
2016-01-17 20:00:44 +0300

    обновлен скрипт

--- 4/Индивидуальные/top5.sql
+++ 4/Индивидуальные/top5.sql
@@ -1,8 +1,15 @@
-create view top5stations as select first 5
-station.name, (count(ticket.start_st) + count(ticket.stop_st))/2 as nums
-from station, ticket
-where station.name=ticket.start_st or station.name=ticket.stop_st
-group by station.name order by nums desc;
+create view top5stations
+as
+select first 5 T1.st as st_name, T1.num + T2.num_start as summ from
+(select ticket.stop_st as st, count(all ticket.stop_st) as num
+from ticket
+group by st order by num desc)T1,
+(select ticket.start_st as st_start, count(all ticket.start_st) as num_start
+from ticket
+group by st_start order by num_start desc)T2
+where T1.st = T2.st_start
+order by summ desc;
+
 commit;
 
 
